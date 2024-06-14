c311723f823f48daae5a0b19ddb807623d36cf0e
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-06-14 20:31:24 +0530

    updated android app

--- app/src/main/java/com/internshala/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/internshala/activitylifecycle/MainActivity.kt
@@ -8,6 +8,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("Onstart called!");
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("OnResume called!");
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause called!");
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onPause called!");
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart called!");
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy called!");
     }
 }
+
+
 
